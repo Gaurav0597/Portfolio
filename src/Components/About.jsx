379f4c13e82c146ab301ef9225fcad5583dc8d04
GauravPetkar <gauravpetkar28@gmail.com>
2022-07-28 18:54:59 +0530

    Name changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,7 @@ export default function About() {
         <Cont>
           <Left>
             <Para>Hi I'm</Para>
-            <Header>Gaurav</Header>
+            <Header>Gaurav Petkar</Header>
             <Para>Full-Stack Web Developer, living in Maharashtra (India)</Para>
             <Link to="footer" smooth={true} duration={1500}>
               <ContactBtn>Contact me</ContactBtn>
